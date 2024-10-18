2501f1ef65a563fe80a9487f4a81e2be9fa14f3f
dream2hvn <138209676+dream2hvn@users.noreply.github.com>
2024-10-18 16:34:42 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/13eBgHRxuDsYLoW7YqN29mvXJPu6hQvyw
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
